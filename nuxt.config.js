bdbf27c681db4675fa8ecc5a460eb3a5c24b2152
caiojulian <caiojulian@hotmail.com>
2022-04-13 14:42:04 -0300

    rename project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'challenge-page-404',
+    title: 'challenge-login-compass',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
