e04751f71cfd07f93beb6c9a20e7ea158ebbe89f
caiojulian <caiojulian@hotmail.com>
2022-04-19 18:42:22 -0300

    feat: cardHome component

---
+++ components/home/CardHome.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="card-home">
+    <article class="card-home__article">
+      <p class="card-home__title card-home__title--small">Our mission is</p>
+      <p class="card-home__description">Nossa missão é</p>
+    </article>
+    <article class="card-home__article">
+      <p class="card-home__title">to transform the world</p>
+      <p class="card-home__description">transformar o mundo</p>
+    </article>
+    <article class="card-home__article">
+      <p class="card-home__title">building digital experiences</p>
+      <p class="card-home__description">construindo experiências digitais</p>
+    </article>
+    <article class="card-home__article">
+      <p class="card-home__title">that enable our client’s growth</p>
+      <p class="card-home__description">
+        que permitam o crescimento dos nossos clientes
+      </p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.card-home {
+  display: none;
+}
+
+@media (min-width: 1024px) {
+  .card-home {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    text-align: right;
+    padding-right: 4rem;
+    height: 500px;
+
+    &__title {
+      margin: 0;
+      font-weight: 700;
+      font-size: 4rem;
+      line-height: 5.063rem;
+      color: #c12d18;
+
+      &--small {
+        font-size: 2.25rem;
+        line-height: 2.875rem;
+      }
+    }
+
+    &__description {
+      margin: 0;
+      font-weight: 400;
+      font-size: 1.5rem;
+      line-height: 1.875rem;
+      color: #222222;
+    }
+  }
+}
+</style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="home">
     <HeaderHome />
-    <FigureHome />
+    <main>
+      <section class="home__section-left">
+        <FigureHome />
+      </section>
+      <section class="home__section-right">
+        <CardHome />
+      </section>
+    </main>
     <FooterHome />
   </section>
 </template>
@@ -10,9 +17,10 @@
 import HeaderHome from '~/components/home/HeaderHome.vue'
 import FigureHome from '~/components/home/FigureHome.vue'
 import FooterHome from '~/components/home/FooterHome.vue'
+import CardHome from '~/components/home/CardHome.vue'
 export default {
   name: 'IndexHomePage',
-  components: { HeaderHome, FigureHome, FooterHome },
+  components: { HeaderHome, FigureHome, FooterHome, CardHome },
 }
 </script>
 <style lang="scss" scoped>
@@ -22,5 +30,10 @@ export default {
   max-width: 100%;
   min-height: 100vh;
   font-family: 'Mark Pro', sans-serif;
+  position: relative;
+
+  &___section-left {
+    width: 100%;
+  }
 }
 </style>
