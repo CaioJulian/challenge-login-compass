327455744f4a967b4e99bf4fb5204f8c93beeaef
caiojulian <caiojulian@hotmail.com>
2022-04-19 17:56:23 -0300

    fix: headerHome component

--- components/home/HeaderHome.vue
+++ components/home/HeaderHome.vue
@@ -3,7 +3,11 @@
     <figure class="header-home__figure">
       <img :src="src" alt="logo compass" width="150" height="43" />
     </figure>
-    <section class="header-home__card">
+    <section class="header-home__card-middle">
+      <p class="header-home__clock">11:26</p>
+      <p class="header-home__date">terça-feira, 17 de março de 2022</p>
+    </section>
+    <section class="header-home__card-right">
       <p class="header-home__location">{{ location }}</p>
       <p class="header-home__temp">{{ temp }}°</p>
     </section>
@@ -29,6 +33,9 @@ export default {
 
 <style lang="scss" scoped>
 .header-home {
+  background: linear-gradient(105.96deg, #ffffff 0%, #f0f0f0 97.86%);
+  position: relative;
+  z-index: 1;
   display: flex;
   justify-content: space-between;
   padding: 2rem 1rem;
@@ -39,7 +46,7 @@ export default {
     margin: 0;
   }
 
-  &__card {
+  &__card-right {
     display: flex;
     flex-direction: column;
     align-items: flex-end;
@@ -64,6 +71,30 @@ export default {
       top: -10px;
       left: -10px;
     }
+  }
+
+  &__card-middle {
+    display: none;
+    text-align: center;
+  }
+
+  &__clock {
+    margin: 0;
+    font-weight: 700;
+    font-size: 9rem;
+    line-height: 7.063rem;
+  }
+
+  &__date {
+    margin: 0;
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 1.125rem;
+  }
+}
+@media (min-width: 1024px) {
+  .header-home__card-middle {
+    display: block;
   }
 }
 </style>
